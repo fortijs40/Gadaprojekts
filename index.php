70dbdc1f86b846817fc81d9b672c80515bf610ab
fortijs40 <xralfsx@inbox.lv>
2023-06-06 10:40:21 +0300

    some new stuff

--- index.php
+++ index.php
@@ -11,7 +11,13 @@
   <body>
   <div class="container mt-5" >
     <h1>Ģērboņu minēšana!</h1>
+  </div>
+  <div class="container mt-5" >
     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/COA_LV_Valmiera.svg/86px-COA_LV_Valmiera.svg.png" class="img-fluid" id="gerbonis" alt="valmierasģērbonis">
+    <div class="input-group mb-3">
+        <button class="btn btn-outline-secondary" type="button" id="button">Button</button>
+        <input type="text" class="form-control" placeholder="" aria-describedby="button">
+    </div>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   </body>
