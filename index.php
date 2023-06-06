c2e3c7003de10b75f3e1a3f962795af46745538f
fortijs40 <xralfsx@inbox.lv>
2023-06-06 10:12:34 +0300

    this is new commit

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="Ģērbonis,Latvija,novadi,minēšana,spēle,vārdi,name,PHP,HTML,Bootstrap">
+    <link href="styles.css" rel="stylesheet">
     <title>Ģērboņu minēšana</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
   <body>
   <div class="container mt-5" >
     <h1>Ģērboņu minēšana!</h1>
-    
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/COA_LV_Valmiera.svg/86px-COA_LV_Valmiera.svg.png" class="img-fluid" id="gerbonis" alt="valmierasģērbonis">
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   </body>
